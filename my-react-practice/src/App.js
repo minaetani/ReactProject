c3deb9a78465ef9daaf8415676c4bf880b2f07c6
Minaë Tani <minae.tani@icloud.com>
2022-09-21 12:58:30 +0300

    Update App.js

--- my-react-practice/src/App.js
+++ my-react-practice/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Sure, now I had made some edits.
         </p>
         <a
           className="App-link"
