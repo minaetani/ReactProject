e0020482e3f912821dbc32812cb5dc121b2d3baf
Minaë Tani <minae.tani@icloud.com>
2022-11-01 08:18:48 +0200

    Debugging - App.tes.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Baking Planner', () => {
+test('renders BakingPlanner', () => {
   render(<App />);
-  const linkElement = screen.getByText(/heading/i);
+  const linkElement = screen.getByText(/heading/);
   expect(linkElement).toBeInTheDocument();
 });
