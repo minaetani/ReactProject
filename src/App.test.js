ae240862f382e50e0178d67c3d858cf46ffe85d1
Minaë Tani <minae.tani@icloud.com>
2022-11-01 11:05:42 +0200

    Another debugging try - App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders baking planner', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Sourdough/i);
+  const linkElement = screen.getByText(/Header/i);
   expect(linkElement).toBeInTheDocument();
 });
